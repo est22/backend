7dfdb2c2d7ff8a5b7ed629893682d64cd193f143
est22 <anlia.seoul@gmail.com>
2024-09-27 14:47:17 +0900

    create dummy data

---
+++ nodejs/05/ch05_05/test.sql
@@ -0,0 +1,9 @@
+insert into posts(title, content, author, createdAt) values('공지사항1', '공지사항1', 'System Operator', '2024-09-27');
+
+insert into posts(title, content, author, createdAt) values('공지사항1', '공지사항2', 'System Operator', '2024-09-27');
+
+insert into posts(title, content, author, createdAt) values('공지사항1', '공지사항3', 'System Operator', '2024-09-27');
+
+insert into posts(title, content, author, createdAt) values('공지사항1', '공지사항4', 'System Operator', '2024-09-27');
+
+insert into posts(title, content, author, createdAt) values('공지사항1', '공지사항5', 'System Operator', '2024-09-27');
