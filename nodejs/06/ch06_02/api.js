e9737ad3f1e64fe0685e2d1bc476db16d65ae124
est22 <anlia.seoul@gmail.com>
2024-10-14 11:24:53 +0900

    create DB

--- nodejs/06/ch06_02/api.js
+++ nodejs/06/ch06_02/api.js
@@ -11,6 +11,27 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 
+const create_sql = `
+    CREATE TABLE if not exists posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title VARCHAR(255),
+        content TEXT,
+        author VARCHAR(100),
+        createdAt datetime default current_timestamp,
+        count integer default 0
+    );
+`;
+
+db.serialize(() => {
+  db.run(create_sql); // create post.db
+});
+
+// 1. GET /posts 게시글 목록
+// 2. GET /posts/1 게시글 상세
+// 3. POST /posts 게시글 쓰기
+// 4. PUT /posts/1 게시글 수정
+// 5. DELETE /posts/1 게시글 삭제
+
 app.listen(PORT, () => {
   console.log(`server list listening on port ${PORT}...`);
 });
