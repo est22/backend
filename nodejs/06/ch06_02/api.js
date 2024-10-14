3930a7261edbf6b5455a9a88d7a3e76506efb94e
est22 <anlia.seoul@gmail.com>
2024-10-14 11:26:14 +0900

    basic frames of RESTapi

--- nodejs/06/ch06_02/api.js
+++ nodejs/06/ch06_02/api.js
@@ -27,10 +27,16 @@ db.serialize(() => {
 });
 
 // 1. GET /posts 게시글 목록
+app.get("/posts", (req, res) => {});
 // 2. GET /posts/1 게시글 상세
+app.get("/posts/:id", (req, res) => {});
 // 3. POST /posts 게시글 쓰기
+app.post("/posts", (req, res) => {});
 // 4. PUT /posts/1 게시글 수정
+app.put("/posts/:id", (req, res) => {});
 // 5. DELETE /posts/1 게시글 삭제
+app.delete("/posts/:id", (req, res) => { });
+
 
 app.listen(PORT, () => {
   console.log(`server list listening on port ${PORT}...`);
