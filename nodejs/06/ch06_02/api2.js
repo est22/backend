3044fbda051c4d7f29f81cbad9225b9b106e0140
est22 <anlia.seoul@gmail.com>
2024-10-14 16:40:31 +0900

    /comment

--- nodejs/06/ch06_02/api2.js
+++ nodejs/06/ch06_02/api2.js
@@ -90,7 +90,50 @@ app.put("/posts/:id", (req, res) => {
   }
 });
 
+// 특정 포스트 댓글 추가
+app.post("/posts/:id/comments", (req, res) => {
+  const { content } = req.body; // req.body.content -> content
+  const postId = req.params.id;
+  const stmt = db.prepare("INSERT INTO comments(postId, content) VALUES (?,?)");
+  const result = stmt.run(postId, content);
+  res
+    .status(201)
+    .json({ id: result.lastInsertRowid, postId: postId, content: content });
+});
+// 특정 포스트의 댓글을 가져오고
+app.get("/posts/:id/comments", (req, res) => {
+  const postId = req.params.id;
+  const comments = db
+    .prepare(`select * from comments where postId = ?`)
+    .all(postId);
+  res.json({ comments: comments });
+});
 
+// 댓글 삭제
+app.delete("/comments/:id", (req, res) => {
+  const id = req.params.id;
+  const stmt = db.prepare(`DELETE from comments where id = ?`).run(id);
+
+  if (result.changes) {
+    res.status(204).end();
+  } else {
+    res.status(404).json({ result: "error", message: "cannot delete comment" });
+  }
+});
+
+// 댓글 수정
+app.put("/comments/:id", (req, res) => {
+  // id는 comments의 id
+  const { content } = req.body;
+  const id = req.params.id;
+  const stmt = db.prepare(`update comments set content = ? where id = ?`);
+  const result = stmt.run(content, id);
+  if (result.changes) {
+    res.status(200).json({ result: "success" });
+  } else {
+    res.status(404).json({ result: "error", message: "Comment not found" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`server list listening on port ${PORT}...`);
