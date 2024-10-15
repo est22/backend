754cabfe56a916417f15a5fb92a96dbd6fc77cbc
est22 <anlia.seoul@gmail.com>
2024-10-15 15:10:17 +0900

    SELECT query

--- nodejs/08/ch08_01/app.js
+++ nodejs/08/ch08_01/app.js
@@ -6,12 +6,14 @@ const sequelize = new Sequelize({
 });
 
 // 모델 선언 (사용자 모델을 샘플로 생성) - create table User ()
+// SQL 쿼리 버전
 `   create table User {
         username VARCHAR(100) not null,
         email VARCHAR(100)
     }
     
 `;
+// sequelize 버전
 const User = sequelize.define("User", {
   username: {
     type: DataTypes.STRING,
@@ -26,10 +28,24 @@ const User = sequelize.define("User", {
 (async () => {
   // await를 사용하기 위해서 빈 async 함수를 정의 및 호출
   // 실제 모델 생성, 데이터 생성, 데이터를 가져오는 연습
+
   await sequelize.sync({ force: false });
+  // = insert into Users(username, email) values ('user01', 'user01@gmail.com');
   const user1 = await User.create({
     username: "User01",
     email: "user01@gmail.com",
   });
   console.log(`user created: ${JSON.stringify(user1)}`);
+
+  // = select * from Users;
+  const users = await User.findAll();
+  console.log(`user: ${JSON.stringify(users)}`);
+
+  // select * from Users where username = 'user01'
+  const user = await User.findOne({
+    where: {
+      username: "user01",
+    },
+  });
+    console.log(`user01: ${JSON.stringify(user)}`)
 })();
