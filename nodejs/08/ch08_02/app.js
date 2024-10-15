14b0f36349fd01c4aa5d85c15abaca211f9744cf
est22 <anlia.seoul@gmail.com>
2024-10-15 16:46:06 +0900

    app.post("/posts")

---
+++ nodejs/08/ch08_02/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const models = require("./models");
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.post("/posts", async (req, res) => {
+  const { title, content, author } = req.body;
+  console.log(`title:${title}`);
+  const post = await models.Post.create({
+    title: title,
+    content: content,
+    author: author,
+  });
+  res.status(201).json(post);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}...`);
+  models.sequelize
+    .sync({ force: false })
+    .then(() => {
+      console.log(`DB connected`);
+    })
+    .catch((err) => {
+      console.error(`DB error: ${err}`);
+      process.exit();
+    }); // 서버 내렸다 올라갈때마다 테이블 새로 생성되면 열받으니까 force: false
+});
