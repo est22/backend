8c3975df603f4fb2d6451e43040c83fc5e099e0b
est22 <anlia.seoul@gmail.com>
2024-10-15 14:36:56 +0900

    define model

--- nodejs/08/ch08_01/app.js
+++ nodejs/08/ch08_01/app.js
@@ -5,3 +5,20 @@ const sequelize = new Sequelize({
   storage: "post.db",
 });
 
+// 모델 선언 (사용자 모델을 샘플로 생성) - create table User ()
+`   create table User {
+        username VARCHAR(100) not null,
+        email VARCHAR(100)
+    }
+    
+`
+const User = sequelize.define("User", {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+})
