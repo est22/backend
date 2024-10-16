c387d2aa1c09fa9cf77d9a27ca7e4d916836d577
est22 <anlia.seoul@gmail.com>
2024-10-16 16:17:17 +0900

    add seed data

--- nodejs/08/ch08_02/migrations/20241016060843-create-task.js
+++ nodejs/08/ch08_02/migrations/20241016060843-create-task.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "User",
+          model: "Users",
           key: "id"
         },
         onDelete: "CASCADE",
