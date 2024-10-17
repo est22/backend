4a65c0cb9bfd3956e7751e455ec6219c469a067a
est22 <anlia.seoul@gmail.com>
2024-10-17 14:18:29 +0900

    create models

---
+++ nodejs/10/ch10_03/models/post.js
@@ -0,0 +1,24 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    static associate(models) {
+      Post.belongsTo(models.User, {
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+      });
+    }
+  }
+  Post.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
+  return Post;
+};
