5fcbcb9affe00b4bc87fc0cc82c94862513a15c2
est22 <anlia.seoul@gmail.com>
2024-10-17 15:35:24 +0900

    controller frame

---
+++ nodejs/10/ch10_03/controllers/postController.js
@@ -0,0 +1,19 @@
+const postService = require("../services/postService");
+
+const createPost = async (req, res) => {};
+const findPostById = async (req, res) => {};
+const findAllPost = async (req, res) => {};
+const updatePost = async (req, res) => {};
+const deletePost = async (req, res) => {};
+
+
+module.exports = {
+  createPost,
+  findPostById,
+  findAllPost,
+  updatePost,
+  deletePost,
+};
+
+
+
