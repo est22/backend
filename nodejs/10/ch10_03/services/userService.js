d03f691240414e27668a718cea1ce64f93a93c27
est22 <anlia.seoul@gmail.com>
2024-10-18 10:37:39 +0900

    userService: createUser, findUserByEmail

---
+++ nodejs/10/ch10_03/services/userService.js
@@ -0,0 +1,15 @@
+const userDao = require("../dao/userDao");
+
+const createUser = async (data) => {
+    return await userDao.createUser(data);
+}
+
+
+const findUserByEmail = async (email) => {
+    return await userDao.findUserByEmail(email);
+}
+
+module.exports = {
+    createUser,
+    findUserByEmail
+}
